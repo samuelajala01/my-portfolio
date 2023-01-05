0232d2965bf09ad44ab4392b60ac8e3d996ef75e
Samuel Ajala <samuelajala01@gmail.com>
2023-01-05 17:28:39 +0000

    chore: added aboutsection to aboutpage

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,9 +1,11 @@
 import Footer from "../Components/Footer";
+import AboutSection from "../Components/AboutSection";
 
 const AboutPage = () => {
   return (
     <>
       <h1>About Page</h1>
+      <AboutSection />
       <Footer />
     </>
   );
