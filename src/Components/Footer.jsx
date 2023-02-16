285371bac37110285970c1b50330ed91a279f43a
Samuel Ajala <samuelajala01@gmail.com>
2023-02-16 00:12:25 +0000

    feat: added visit count functionality

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Footer = () => {
+  const [visits, setVisits] = useState(0);
+
+  useEffect(() => {
+    axios
+      .get("https://api.countapi.xyz/hit/samuelajala.vercel.app/visits")
+      .then((response) => {
+        // Check if the response data is already an object
+        const data =
+          typeof response.data === "string"
+            ? JSON.parse(response.data)
+            : response.data;
+        setVisits(data.value);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <footer>
       <p>
         &copy; <span id="footer-year"></span> Samuel Ajala
       </p>
-      <p>site views: ___</p>
+      <p>site views: {visits}</p>
     </footer>
   );
 };
