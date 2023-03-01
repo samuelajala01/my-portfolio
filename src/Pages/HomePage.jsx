7334404a2db423d605a55c83f48c243d48ed6ea5
Samuel Ajala <samuelajala01@gmail.com>
2023-03-01 22:42:28 +0000

    fix: added AboutSection, changed page bg

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import ContactPage from "./ContactPage";
 import ProjectSection from "../Components/ProjectSection";
 import HomeSection from "../Components/HomeSection";
+import AboutSection from "../Components/AboutSection"
 import ServicesSection from "../Components/ServicesSection";
 
 const HomePage = () => {
